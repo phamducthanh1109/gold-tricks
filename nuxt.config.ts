90e2a51b380e6ad4ba0dcaa93c9ada8d0e5f765e
Thanh Pham <thanhpd@miichisoft.net>
2024-08-14 17:25:08 +0700

    setup eslint & prettier autoformat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,21 @@
 export default defineNuxtConfig({
-  ssr: false,
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  typescript: {
-    typeCheck: true,
-  },
-  build: {
-    transpile: ['vuetify'],
-  },
-  eslint: {
-    config: {
-      stylistic: true,
-    },
-    checker: false,
-  },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui',
-  ],
-  colorMode: {
-    preference: 'light',
-  },
-})
+	ssr: false,
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	typescript: {
+		typeCheck: true,
+	},
+	build: {
+		transpile: ['vuetify'],
+	},
+	eslint: {
+		config: {
+			stylistic: true,
+		},
+		checker: false,
+	},
+	modules: ['@nuxt/eslint', '@nuxt/ui'],
+	colorMode: {
+		preference: 'light',
+	},
+});
