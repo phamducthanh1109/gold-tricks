febb56120701f3b2d41a5b1528ccded7fd6f4a2f
Thanh Pham <thanhpd@miichisoft.net>
2024-09-12 16:24:14 +0700

    config(prettier): update rule

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-	<UContainer class="my-5">
-		<AutoResponsiveGrid />
-	</UContainer>
+  <UContainer class="my-5">
+    <AutoResponsiveGrid />
+  </UContainer>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,28 @@
 export default defineNuxtConfig({
-	ssr: false,
-	compatibilityDate: '2024-04-03',
-	devtools: { enabled: true },
-	typescript: {
-		typeCheck: true,
-	},
-	build: {
-		transpile: ['vuetify'],
-	},
-	eslint: {
-		config: {
-			stylistic: true,
-		},
-		checker: false,
-	},
-	modules: ['@nuxt/eslint', '@nuxt/ui'],
-	css: ['@/assets/css/main.css', '@/assets/scss/global.scss'],
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData: '@use "@/assets/scss/_mixins.scss" as *;',
-				},
-			},
-		},
-	},
+  ssr: false,
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+  },
+  build: {
+    transpile: ['vuetify'],
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+    checker: false,
+  },
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  css: ['@/assets/css/main.css', '@/assets/scss/global.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_mixins.scss" as *;',
+        },
+      },
+    },
+  },
 });
